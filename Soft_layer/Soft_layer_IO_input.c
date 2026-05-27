4c6e5d578607230ecb4555ea7d4fa50df7bb0fc4
easonyzw <101691537+easonyzw@users.noreply.github.com>
2026-05-27 08:59:23 +0800

    refactor: simplify IO state update with channel table

--- Soft_layer/Soft_layer_IO_input.c
+++ Soft_layer/Soft_layer_IO_input.c
@@ -2,713 +2,200 @@
 
 sIO_parameter IO_parameter[16];
 
+/*
+ * 16 è·¯è¾“å…¥é€šé“å’Œ 16 è·¯ LED çš„å¯¹åº”å…³ç³»è¡¨ã€‚
+ *
+ * åŽŸæ¥çš„ IO_state_update() æŠŠ CH0 ~ CH15 åˆ†åˆ«å†™äº† 16 æ®µ if/elseï¼Œé€»è¾‘å®Œå…¨é‡å¤ã€‚
+ * è¿™é‡Œæ”¹æˆâ€œè¡¨é©±åŠ¨â€ï¼šé€šé“çš„ç¡¬ä»¶å·®å¼‚æ”¾åœ¨è¡¨é‡Œï¼Œå¤„ç†æµç¨‹ç»Ÿä¸€æ”¾åˆ°å¾ªçŽ¯é‡Œã€‚
+ *
+ * æ³¨æ„ï¼šæœ¬æ¿å­çš„ LED ä¸ºä½Žç”µå¹³ç‚¹äº®ï¼Œæ‰€ä»¥ IO_led_set() å†…éƒ¨ä½¿ç”¨ GPIO_ResetBits() ç‚¹äº® LEDã€‚
+ */
+typedef struct
+{
+    GPIO_TypeDef *input_port;
+    unsigned short input_pin;
+
+    GPIO_TypeDef *led_port;
+    unsigned short led_pin;
+
+    unsigned char input_byte;  /* 0: IO_Input_L, 1: IO_Input_H */
+    unsigned char bit_mask;    /* å¯¹åº”è¾“å…¥çŠ¶æ€å­—èŠ‚ä¸­çš„ bit ä½ */
+} sIO_channel_config;
+
+static const sIO_channel_config IO_channel_table[16] =
+{
+    /* input port, input pin,  led port, led pin,  byte, bit */
+    {GPIOC, GPIO_Pin_6,  GPIOC, GPIO_Pin_2,  0, 0x01},  /* CH0  -> LED0  */
+    {GPIOD, GPIO_Pin_2,  GPIOA, GPIO_Pin_0,  0, 0x02},  /* CH1  -> LED1  */
+    {GPIOB, GPIO_Pin_15, GPIOA, GPIO_Pin_2,  0, 0x04},  /* CH2  -> LED2  */
+    {GPIOB, GPIO_Pin_3,  GPIOA, GPIO_Pin_4,  0, 0x08},  /* CH3  -> LED3  */
+    {GPIOA, GPIO_Pin_11, GPIOA, GPIO_Pin_6,  0, 0x10},  /* CH4  -> LED4  */
+    {GPIOC, GPIO_Pin_12, GPIOC, GPIO_Pin_4,  0, 0x20},  /* CH5  -> LED5  */
+    {GPIOA, GPIO_Pin_8,  GPIOB, GPIO_Pin_1,  0, 0x40},  /* CH6  -> LED6  */
+    {GPIOB, GPIO_Pin_14, GPIOB, GPIO_Pin_10, 0, 0x80},  /* CH7  -> LED7  */
+
+    {GPIOA, GPIO_Pin_15, GPIOC, GPIO_Pin_3,  1, 0x01},  /* CH8  -> LED8  */
+    {GPIOC, GPIO_Pin_9,  GPIOA, GPIO_Pin_1,  1, 0x02},  /* CH9  -> LED9  */
+    {GPIOF, GPIO_Pin_7,  GPIOA, GPIO_Pin_3,  1, 0x04},  /* CH10 -> LED10 */
+    {GPIOC, GPIO_Pin_8,  GPIOA, GPIO_Pin_5,  1, 0x08},  /* CH11 -> LED11 */
+    {GPIOF, GPIO_Pin_6,  GPIOA, GPIO_Pin_7,  1, 0x10},  /* CH12 -> LED12 */
+    {GPIOB, GPIO_Pin_13, GPIOB, GPIO_Pin_0,  1, 0x20},  /* CH13 -> LED13 */
+    {GPIOC, GPIO_Pin_7,  GPIOB, GPIO_Pin_2,  1, 0x40},  /* CH14 -> LED14 */
+    {GPIOB, GPIO_Pin_12, GPIOB, GPIO_Pin_11, 1, 0x80}   /* CH15 -> LED15 */
+};
+
+static void IO_led_set(unsigned char channel, unsigned char on)
+{
+    if(on)
+    {
+        GPIO_ResetBits(IO_channel_table[channel].led_port, IO_channel_table[channel].led_pin);
+    }
+    else
+    {
+        GPIO_SetBits(IO_channel_table[channel].led_port, IO_channel_table[channel].led_pin);
+    }
+}
+
+static void IO_status_bit_set(unsigned char channel, unsigned char high_level)
+{
+    unsigned char clear_mask;
+
+    clear_mask = (unsigned char)(~IO_channel_table[channel].bit_mask);
+
+    if(IO_channel_table[channel].input_byte == 0)
+    {
+        if(high_level)
+        {
+            IO_Input_L |= IO_channel_table[channel].bit_mask;
+        }
+        else
+        {
+            IO_Input_L &= clear_mask;
+        }
+    }
+    else
+    {
+        if(high_level)
+        {
+            IO_Input_H |= IO_channel_table[channel].bit_mask;
+        }
+        else
+        {
+            IO_Input_H &= clear_mask;
+        }
+    }
+}
+
+static void IO_channel_update(unsigned char channel)
+{
+    unsigned char input_state;
+
+    if(timer_counter.IO_filter[channel] < IO_parameter[channel].filter)
+    {
+        return;
+    }
+
+    input_state = GPIO_ReadInputDataBit(IO_channel_table[channel].input_port,
+                                        IO_channel_table[channel].input_pin);
+
+    if(input_state == SET)
+    {
+        IO_led_set(channel, 1);
+        IO_status_bit_set(channel, 1);
+    }
+    else
+    {
+        IO_led_set(channel, 0);
+        IO_status_bit_set(channel, 0);
+    }
+
+    timer_counter.IO_filter[channel] = 0;
+}
+
 /******************************************************************
  * @brief  digital input data initialize
  * @input  none
- * @return  none
-******************************************************************/
+ * @return none
+ ******************************************************************/
 void digital_input_initialization(void)
 {
-  unsigned char i;
-		
-	for(i=0;i<16;i++)
-	{
-			IO_parameter[i].filter=1;
-	}
-	
+    unsigned char i;
+
+    for(i = 0; i < 16; i++)
+    {
+        IO_parameter[i].filter = 1;
+    }
 }
 
-
 /******************************************************************
- * @brief  Port LED 
+ * @brief  Update 16-channel input status and related LEDs
  * @input  none
- * @return  none
-******************************************************************/
-void IO_state_update(void)                          
+ * @return none
+ ******************************************************************/
+void IO_state_update(void)
 {
+    unsigned char i;
 
-	 if(CH0 == SET)   // µ±Ç°¼ì²âµ½ CH0 ÊÇ¸ßµçÆ½
+    for(i = 0; i < 16; i++)
     {
-        // ===== µÚ¶þ²½£ºÂË²¨Ê±¼äÊÇ·ñ´ïµ½ãÐÖµ =====
-        if(timer_counter.IO_filter[0] >= IO_parameter[0].filter) // ÊÇ·ñÒÑ¾­ÎÈ¶¨ÁËÒ»¶ÎÊ±¼ä£¨ÀýÈç ¡Ý5ms£©
-        { 
-            // ===== µÚÈý²½£ºÔÙ´Î¶ÁÈ¡È·ÈÏ£¨·À¶¶£© =====
-            if(CH0 == SET)
-            {
-                // ===== È·ÈÏÎÈ¶¨Îª¸ßµçÆ½ =====
-                LED0_ON;                // µãÁÁ LED
-                IO_Input_L |= 0x01;     // ÉèÖÃÊäÈë×´Ì¬ bit0 = 1
-            }
-            else
-            {
-                // ===== ³öÏÖ¶¶¶¯£¨¸Õ²ÅÊÇ¸ß£¬ÏÖÔÚ±äµÍ£©=====
-                LED0_OFF;
-                IO_Input_L &= 0xFE;     // Çå bit0
-            }
-
-            // ===== µÚËÄ²½£º±¾ÂÖÂË²¨Íê³É£¬¼ÆÊ±ÇåÁã =====
-            timer_counter.IO_filter[0] = 0;
-        }
-    }
-    else   // µ±Ç°¼ì²âµ½ CH0 ÊÇµÍµçÆ½
-    {
-        // ===== Í¬ÑùÁ÷³Ì£¬Ö»ÊÇÕë¶Ô¡°µÍµçÆ½È·ÈÏ¡± =====
-        if(timer_counter.IO_filter[0] >= IO_parameter[0].filter)
-        { 
-            // ÔÙÈ·ÈÏÒ»´Î
-            if(CH0 == RESET)
-            {
-                // ===== È·ÈÏÎÈ¶¨ÎªµÍµçÆ½ =====
-                LED0_OFF;               
-                IO_Input_L &= 0xFE;     // Çå bit0
-            }
-            else
-            {
-                // ===== ³öÏÖ¶¶¶¯£¨¸Õ²ÅÊÇµÍ£¬ÏÖÔÚ±ä¸ß£©=====
-                LED0_ON;                
-                IO_Input_L |= 0x01;     // ÉèÖÃ bit0
-            }
-
-            // ===== ±¾ÂÖÂË²¨½áÊø£¬ÖØÐÂ¿ªÊ¼¼ÆÊ± =====
-            timer_counter.IO_filter[0] = 0;
-        }     
-    }
-    
-    
-    
-    if(CH1==SET)
-		{
-      if(timer_counter.IO_filter[1]>=IO_parameter[1].filter)
-      { 
-        if(CH1==SET)
-        {
-          LED1_ON;
-          IO_Input_L|=0x02;                  
-        }
-        else
-        {
-          LED1_OFF;
-          IO_Input_L&=0xFD;
-        }
-        timer_counter.IO_filter[1]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[1]>=IO_parameter[1].filter)
-      { 
-        if(CH1==RESET)
-        {
-          LED1_OFF;
-          IO_Input_L&=0xFD;                  
-        }
-        else
-        {
-          LED1_ON;
-          IO_Input_L|=0x02;
-        }
-        timer_counter.IO_filter[1]=0;
-      }     
-    }
-    
-    if(CH2==SET)
-		{
-      if(timer_counter.IO_filter[2]>=IO_parameter[2].filter)
-      { 
-        if(CH2==SET)
-        {
-          LED2_ON;
-          IO_Input_L|=0x04;                  
-        }
-        else
-        {
-          LED2_OFF;
-          IO_Input_L&=0xFB;
-        }
-        timer_counter.IO_filter[2]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[2]>=IO_parameter[2].filter)
-      { 
-        if(CH2==RESET)
-        {
-          LED2_OFF;
-          IO_Input_L&=0xFB;                  
-        }
-        else
-        {
-          LED2_ON;
-          IO_Input_L|=0x04;
-        }
-        timer_counter.IO_filter[2]=0;
-      }     
-    }
-    
-    
-    if(CH3==SET)
-		{
-      if(timer_counter.IO_filter[3]>=IO_parameter[3].filter)
-      { 
-        if(CH3==SET)
-        {
-          LED3_ON;
-          IO_Input_L|=0x08;                  
-        }
-        else
-        {
-          LED3_OFF;
-          IO_Input_L&=0xF7;
-        }
-        timer_counter.IO_filter[3]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[3]>=IO_parameter[3].filter)
-      { 
-        if(CH3==RESET)
-        {
-          LED3_OFF;
-          IO_Input_L&=0xF7;                  
-        }
-        else
-        {
-          LED3_ON;
-          IO_Input_L|=0x08;
-        }
-        timer_counter.IO_filter[3]=0;
-      }     
-    }
-    
-    
-    if(CH4==SET)
-		{
-      if(timer_counter.IO_filter[4]>=IO_parameter[4].filter)
-      { 
-        if(CH4==SET)
-        {
-          LED4_ON;
-          IO_Input_L|=0x10;                  
-        }
-        else
-        {
-          LED4_OFF;
-          IO_Input_L&=0xEF;
-        }
-        timer_counter.IO_filter[4]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[4]>=IO_parameter[4].filter)
-      { 
-        if(CH4==RESET)
-        {
-          LED4_OFF;
-          IO_Input_L&=0xEF;                  
-        }
-        else
-        {
-          LED4_ON;
-          IO_Input_L|=0x10;
-        }
-        timer_counter.IO_filter[4]=0;
-      }     
+        IO_channel_update(i);
     }
 
-    if(CH5==SET)
-		{
-      if(timer_counter.IO_filter[5]>=IO_parameter[5].filter)
-      { 
-        if(CH5==SET)
-        {
-          LED5_ON;
-          IO_Input_L|=0x20;                  
-        }
-        else
-        {
-          LED5_OFF;
-          IO_Input_L&=0xDF;
-        }
-        timer_counter.IO_filter[5]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[5]>=IO_parameter[5].filter)
-      { 
-        if(CH5==RESET)
-        {
-          LED5_OFF;
-          IO_Input_L&=0xDF;                  
-        }
-        else
-        {
-          LED5_ON;
-          IO_Input_L|=0x20;
-        }
-        timer_counter.IO_filter[5]=0;
-      }     
-    }
-    
-    
-    if(CH6==SET)
-		{
-      if(timer_counter.IO_filter[6]>=IO_parameter[6].filter)
-      { 
-        if(CH6==SET)
-        {
-          LED6_ON;
-          IO_Input_L|=0x40;                  
-        }
-        else
-        {
-          LED6_OFF;
-          IO_Input_L&=0xBF;
-        }
-        timer_counter.IO_filter[6]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[6]>=IO_parameter[6].filter)
-      { 
-        if(CH6==RESET)
-        {
-          LED6_OFF;
-          IO_Input_L&=0xBF;                  
-        }
-        else
-        {
-          LED6_ON;
-          IO_Input_L|=0x40;
-        }
-        timer_counter.IO_filter[6]=0;
-      }     
-    }
-    
-    
-    if(CH7==SET)
-		{
-      if(timer_counter.IO_filter[7]>=IO_parameter[7].filter)
-      { 
-        if(CH7==SET)
-        {
-          LED7_ON;
-          IO_Input_L|=0x80;                  
-        }
-        else
-        {
-          LED7_OFF;
-          IO_Input_L&=0x7F;
-        }
-        timer_counter.IO_filter[7]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[7]>=IO_parameter[7].filter)
-      { 
-        if(CH7==RESET)
-        {
-          LED7_OFF;
-          IO_Input_L&=0x7F;                  
-        }
-        else
-        {
-          LED7_ON;
-          IO_Input_L|=0x80;
-        }
-        timer_counter.IO_filter[7]=0;
-      }     
-    }
-    
-    
-    if(CH8==SET)
-		{
-      if(timer_counter.IO_filter[8]>=IO_parameter[8].filter)
-      { 
-        if(CH8==SET)
-        {
-          LED8_ON;
-          IO_Input_H|=0x01;                  
-        }
-        else
-        {
-          LED8_OFF;
-          IO_Input_H&=0xFE;
-        }
-        timer_counter.IO_filter[8]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[8]>=IO_parameter[8].filter)
-      { 
-        if(CH8==RESET)
-        {
-          LED8_OFF;
-          IO_Input_H&=0xFE;                  
-        }
-        else
-        {
-          LED8_ON;
-          IO_Input_H|=0x01;
-        }
-        timer_counter.IO_filter[8]=0;
-      }     
-    }
-    
-    if(CH9==SET)
-		{
-      if(timer_counter.IO_filter[9]>=IO_parameter[9].filter)
-      { 
-        if(CH9==SET)
-        {
-          LED9_ON;
-          IO_Input_H|=0x02;                  
-        }
-        else
-        {
-          LED9_OFF;
-          IO_Input_H&=0xFD;
-        }
-        timer_counter.IO_filter[9]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[9]>=IO_parameter[9].filter)
-      { 
-        if(CH9==RESET)
-        {
-          LED9_OFF;
-          IO_Input_H&=0xFD;                  
-        }
-        else
-        {
-          LED9_ON;
-          IO_Input_H|=0x02;
-        }
-        timer_counter.IO_filter[9]=0;
-      }     
-    }
-    
-    
-    
-    if(CH10==SET)
-		{
-      if(timer_counter.IO_filter[10]>=IO_parameter[10].filter)
-      { 
-        if(CH10==SET)
-        {
-          LED10_ON;
-          IO_Input_H|=0x04;                  
-        }
-        else
-        {
-          LED10_OFF;
-          IO_Input_H&=0xFB;
-        }
-        timer_counter.IO_filter[10]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[10]>=IO_parameter[10].filter)
-      { 
-        if(CH10==RESET)
-        {
-          LED10_OFF;
-          IO_Input_H&=0xFB;                  
-        }
-        else
-        {
-          LED10_ON;
-          IO_Input_H|=0x04;
-        }
-        timer_counter.IO_filter[10]=0;
-      }     
-    }
-    
-    
-    if(CH11==SET)
-		{
-      if(timer_counter.IO_filter[11]>=IO_parameter[11].filter)
-      { 
-        if(CH11==SET)
-        {
-          LED11_ON;
-          IO_Input_H|=0x08;                  
-        }
-        else
-        {
-          LED11_OFF;
-          IO_Input_H&=0xF7;
-        }
-        timer_counter.IO_filter[11]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[11]>=IO_parameter[11].filter)
-      { 
-        if(CH11==RESET)
-        {
-          LED11_OFF;
-          IO_Input_H&=0xF7;                  
-        }
-        else
-        {
-          LED11_ON;
-          IO_Input_H|=0x08;
-        }
-        timer_counter.IO_filter[11]=0;
-      }     
-    }
-    
-    
-    
-    if(CH12==SET)
-		{
-      if(timer_counter.IO_filter[12]>=IO_parameter[12].filter)
-      { 
-        if(CH12==SET)
-        {
-          LED12_ON;
-          IO_Input_H|=0x10;                  
-        }
-        else
-        {
-          LED12_OFF;
-          IO_Input_H&=0xEF;
-        }
-        timer_counter.IO_filter[12]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[12]>=IO_parameter[12].filter)
-      { 
-        if(CH12==RESET)
-        {
-          LED12_OFF;
-          IO_Input_H&=0xEF;                  
-        }
-        else
-        {
-          LED12_ON;
-          IO_Input_H|=0x10;
-        }
-        timer_counter.IO_filter[12]=0;
-      }     
-    }
-    
-    
-    
-    if(CH13==SET)
-		{
-      if(timer_counter.IO_filter[13]>=IO_parameter[13].filter)
-      { 
-        if(CH13==SET)
-        {
-          LED13_ON;
-          IO_Input_H|=0x20;                  
-        }
-        else
-        {
-          LED13_OFF;
-          IO_Input_H&=0xDF;
-        }
-        timer_counter.IO_filter[13]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[13]>=IO_parameter[13].filter)
-      { 
-        if(CH13==RESET)
-        {
-          LED13_OFF;
-          IO_Input_H&=0xDF;                  
-        }
-        else
-        {
-          LED13_ON;
-          IO_Input_H|=0x20;
-        }
-        timer_counter.IO_filter[13]=0;
-      }     
-    }
-    
-    
-    if(CH14==SET)
-		{
-      if(timer_counter.IO_filter[14]>=IO_parameter[14].filter)
-      { 
-        if(CH14==SET)
-        {
-          LED14_ON;
-          IO_Input_H|=0x40;                  
-        }
-        else
-        {
-          LED14_OFF;
-          IO_Input_H&=0xBF;
-        }
-        timer_counter.IO_filter[14]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[14]>=IO_parameter[14].filter)
-      { 
-        if(CH14==RESET)
-        {
-          LED14_OFF;
-          IO_Input_H&=0xBF;                  
-        }
-        else
-        {
-          LED14_ON;
-          IO_Input_H|=0x40;
-        }
-        timer_counter.IO_filter[14]=0;
-      }     
-    }
-    
-    
-    if(CH15==SET)
-		{
-      if(timer_counter.IO_filter[15]>=IO_parameter[15].filter)
-      { 
-        if(CH15==SET)
-        {
-          LED15_ON;
-          IO_Input_H|=0x80;                  
-        }
-        else
-        {
-          LED15_OFF;
-          IO_Input_H&=0x7F;
-        }
-        timer_counter.IO_filter[15]=0;
-      }
-		}
-    else
-    {
-      
-      if(timer_counter.IO_filter[15]>=IO_parameter[15].filter)
-      { 
-        if(CH15==RESET)
-        {
-          LED15_OFF;
-          IO_Input_H&=0x7F;                  
-        }
-        else
-        {
-          LED15_ON;
-          IO_Input_H|=0x80;
-        }
-        timer_counter.IO_filter[15]=0;
-      }     
-    }
-   
-		module_show_status_running();
-
+    module_show_status_running();
 }
 
 /******************************************************************
  * @brief  LED status of station number waiting
  * @input  none
- * @return  none
-******************************************************************/
+ * @return none
+ ******************************************************************/
 void module_show_status(void)
 {
-  if(timer_counter.LED_ms>500)
-  {
-    LED_all_close();
-    timer_counter.LED_ms=0;
-  }
-  else if(timer_counter.LED_ms>250)
-  {    
-    LED_all_open();
-  }
+    if(timer_counter.LED_ms > 500)
+    {
+        LED_all_close();
+        timer_counter.LED_ms = 0;
+    }
+    else if(timer_counter.LED_ms > 250)
+    {
+        LED_all_open();
+    }
 }
 
 void module_show_status_running(void)
 {
-  if((IO_Input_L!=0)||(IO_Input_H!=0))
-  {
-    return;
-  }
-  
-  if(timer_counter.LED_ms>5200)
-  {
-    LED_all_close();
-    timer_counter.LED_ms=0;
-  }
-  else if(timer_counter.LED_ms>5000)
-  {    
-    LED_all_open();
-  }
+    if((IO_Input_L != 0) || (IO_Input_H != 0))
+    {
+        return;
+    }
+
+    if(timer_counter.LED_ms > 5200)
+    {
+        LED_all_close();
+        timer_counter.LED_ms = 0;
+    }
+    else if(timer_counter.LED_ms > 5000)
+    {
+        LED_all_open();
+    }
 }
 
 void LED_all_open(void)
 {
-  PAout(7)=0;
-  PAout(6)=0;
-  PAout(5)=0;
-  PAout(4)=0;
-  PAout(3)=0;
-  PAout(2)=0;
-  PAout(1)=0;
-  PAout(0)=0;
-  PBout(0)=0;
-  PBout(1)=0;
-  PBout(2)=0;
-  PBout(10)=0;
-  PBout(11)=0;
-  PCout(2)=0;
-  PCout(3)=0;
-  PCout(4)=0;
+    unsigned char i;
 
-	
+    for(i = 0; i < 16; i++)
+    {
+        IO_led_set(i, 1);
+    }
 }
+
 void LED_all_close(void)
 {
-	PAout(7)=1;
-  PAout(6)=1;
-  PAout(5)=1;
-  PAout(4)=1;
-  PAout(3)=1;
-  PAout(2)=1;
-  PAout(1)=1;
-  PAout(0)=1;
-  PBout(0)=1;
-  PBout(1)=1;
-  PBout(2)=1;
-  PBout(10)=1;
-  PBout(11)=1;
-  PCout(2)=1;
-  PCout(3)=1;
-  PCout(4)=1;
+    unsigned char i;
+
+    for(i = 0; i < 16; i++)
+    {
+        IO_led_set(i, 0);
+    }
 }
-
-
